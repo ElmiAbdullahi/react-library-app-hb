5ba07b836fe1e1a49d20426a97a69bf08b8cec6f
Abdullahi Elmi <abdullahielmi147@gmail.com>
2023-01-23 18:37:09 -0800

    built /books/:id route which should return a detail

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
       <h1>Library Catalog</h1>
       <Switch>
         <Route exact path="/books" component={BookList} />
+        <Route exact path="/books/:id" component={BookDetail} />
       </Switch>
     </main>
   );
